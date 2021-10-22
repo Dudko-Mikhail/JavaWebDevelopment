d4902745b0fc69aa383b7845e549586cc866f95b
Dudko Mikhail <dudkomikhail01@gmail.com>
2021-10-22 10:41:43 +0300

    update factory

--- shapes/src/main/java/by/bsu/shapetask/factory/TriangleFactory.java
+++ shapes/src/main/java/by/bsu/shapetask/factory/TriangleFactory.java
@@ -14,7 +14,15 @@ public class TriangleFactory {
         return instance;
     }
 
-    public Triangle getTriangle(String name, Point a, Point b, Point c) throws ShapeException {
+    public Triangle createTriangle(String name, Point a, Point b, Point c) throws ShapeException {
+        return new Triangle(name, a, b, c);
+    }
+
+    public Triangle createTriangle(String name, double x1, double y1, double x2,
+                                   double y2, double x3, double y3) throws ShapeException {
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        Point c = new Point(x3, y3);
         return new Triangle(name, a, b, c);
     }
 
